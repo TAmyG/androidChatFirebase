b06bd9cc97f8b3ed65d2cb54f4a68fb1fb410441
Your Name <tamy.vivas@gmail.com>
2016-06-09 21:39:59 -0600

    Implementación de interfaz LoginView dentro de MainActivity correspondiente a la capa de Vista.

---
+++ app/src/main/java/com/distinct/tamyg/androidchat/contactList/ContactListActivity.java
@@ -0,0 +1,15 @@
+package com.distinct.tamyg.androidchat.contactList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.distinct.tamyg.androidchat.R;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
--- app/src/main/java/com/distinct/tamyg/androidchat/login/MainActivity.java
+++ app/src/main/java/com/distinct/tamyg/androidchat/login/MainActivity.java
@@ -1,6 +1,8 @@
 package com.distinct.tamyg.androidchat.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,12 +12,13 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 import com.distinct.tamyg.androidchat.R;
+import com.distinct.tamyg.androidchat.contactList.ContactListActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginView{
 
     @Bind(R.id.editTxtEmail)
     EditText editTxtEmail;
@@ -29,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
     @Bind(R.id.layoutMainContainer)
     RelativeLayout layoutMainContainer;
+
+    //presentador
+    private LoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +51,69 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btnSignup:
                 break;
         }
+    }
+
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        setInputs(false);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btnSignup)
+    @Override
+    public void handleSignUp() {
+        loginPresenter.registerNewUser(editTxtEmail.getText().toString(),
+                editTxtPassword.getText().toString());
+    }
+
+    @Override
+    public void handleSignIn() {
+        loginPresenter.validateLogin(editTxtEmail.getText().toString(),
+                editTxtPassword.getText().toString());
+    }
+
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void loginError(String error) {
+        editTxtPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signin), error);
+        editTxtPassword.setText(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        Snackbar.make(layoutMainContainer, R.string.login_notice_message_useradded, Snackbar.LENGTH_SHORT ).show();
+    }
+
+    @Override
+    public void newUserError(String error) {
+        editTxtPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup), error);
+        editTxtPassword.setText(msgError);
+    }
+
+    private void setInputs(Boolean enabled){
+        editTxtEmail.setEnabled(enabled);
+        editTxtPassword.setEnabled(enabled);
+        btnSignin.setEnabled(enabled);
+        btnSignup.setEnabled(enabled);
     }
 }
