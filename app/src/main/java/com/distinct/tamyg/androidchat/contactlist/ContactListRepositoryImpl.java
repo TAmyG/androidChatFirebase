a7e3e878fa4bad05c8ec86a158c1dcb633eff49b
Your Name <tamy.vivas@gmail.com>
2016-06-12 15:05:50 -0600

    Implementación de repositorio ContactListRepository

--- app/src/main/java/com/distinct/tamyg/androidchat/contactlist/ContactListRepositoryImpl.java
+++ app/src/main/java/com/distinct/tamyg/androidchat/contactlist/ContactListRepositoryImpl.java
@@ -1,41 +1,101 @@
 package com.distinct.tamyg.androidchat.contactlist;
 
+import com.distinct.tamyg.androidchat.contactlist.event.ContactListEvent;
+import com.distinct.tamyg.androidchat.domain.FirebaseHelper;
+import com.distinct.tamyg.androidchat.entities.User;
+import com.distinct.tamyg.androidchat.lib.EventBus;
+import com.distinct.tamyg.androidchat.lib.GreenRobotEventBus;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+
 /**
  * Created by tamyg on 12/06/16.
  */
 public class ContactListRepositoryImpl implements ContactListRepository {
+    private FirebaseHelper  helper;
+    private ChildEventListener contactEventListener;
+
+    public ContactListRepositoryImpl() {
+        helper = FirebaseHelper.getInstance();
+    }
+
     @Override
     public void signOff() {
-
+        helper.signOff();
     }
 
     @Override
     public String getCurrentUserEmail() {
-        return null;
+        return helper.getAuthUserEmail();
     }
 
     @Override
     public void removeContact(String email) {
-
+        String currentUserEmail = helper.getAuthUserEmail();
+        helper.getOneContactReference(currentUserEmail, email).removeValue();//borro a mi contacto
+        helper.getOneContactReference( email, currentUserEmail).removeValue();//me borro de mi contacto
     }
 
     @Override
     public void destroyListener() {
-
+        contactEventListener = null;
     }
 
     @Override
     public void subscribeToContactListEvent() {
+        if(contactEventListener == null) {
+            contactEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    handleContact(dataSnapshot, ContactListEvent.onContactAdded);
+                }
 
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    handleContact(dataSnapshot, ContactListEvent.onContactChanged);
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    handleContact(dataSnapshot, ContactListEvent.onContactRemoved);
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {}
+            };
+        }
+        helper.getMyContactsReference().addChildEventListener(contactEventListener);
+    }
+
+    private void handleContact(DataSnapshot dataSnapshot, int type) {
+        String email = dataSnapshot.getKey();
+        email = email.replace("_",".");
+        boolean online = ((Boolean)dataSnapshot.getValue()).booleanValue();
+        User user = new User(email, online, null);
+        postEvent(type, user);
     }
 
     @Override
     public void onsubscribeToContactListEvent() {
-
+        if(contactEventListener != null){
+            helper.getMyContactsReference().removeEventListener(contactEventListener);
+        }
     }
 
     @Override
     public void changeConnectionStatus(boolean online) {
 
+    }
+
+    private void postEvent(int type, User user) {
+        ContactListEvent contactListEvent = new ContactListEvent();
+        contactListEvent.setEventType(type);
+        contactListEvent.setUser(user);
+        EventBus eventBus = GreenRobotEventBus.getInstance();
+        eventBus.post(contactListEvent);
     }
 }
